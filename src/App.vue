2817be39f4d1ceaf1366d1418a47164daecc81c4
mookiehub <tangzhaobin@hotmail.com>
2024-09-14 16:35:17 +0800

    创建项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="app">App</div>
+</template>
+
+<script>
+export default {
+    name: 'App'
+};
+</script>
+
+<style lang="scss">
+body {
+    padding: 50px;
+    margin: 0;
+}
+</style>
